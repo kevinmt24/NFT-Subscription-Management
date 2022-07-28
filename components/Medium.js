23e6634c7133d45ef3f52dd9c38d2386b5130b09
Kevinmt24 <kevinmt01@gmail.com>
2022-07-29 01:31:07 +0530

    Moved solidity files to utils

--- components/Medium.js
+++ components/Medium.js
@@ -3,7 +3,7 @@ const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
 
 const CONTRACT_ADDRESS = '0xE77C2DF6c51696dd6Aad37D12d8327EC2dbE0cA5'
 
-const { abi } = require("../artifacts/contracts/Medium.sol/Medium.json");
+const { abi } = require("../utils/Medium.json");
 const signer = provider.getSigner();
 
 const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, signer);
